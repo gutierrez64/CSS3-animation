aeaf583b83683580125760b9ab068f27eb4bc76e
gutierrez64 <gutierrezmatheus210@gmail.com>
2021-12-05 16:33:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CSS3-animation
+ Este é um site que fiz para testar meus conhecimentos de CSS3
---
+++ site/index.js
@@ -0,0 +1,4 @@
+function background_white() {
+    document.getElementById("fundo1").style.backgroundColor = "rgba(255, 255, 255, 0.431)";
+    document.getElementById("fundo2").style.backgroundColor = "rgba(255, 255, 255, 0.431)";
+}
